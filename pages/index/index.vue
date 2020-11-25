98d2e5c41c91ed4d46b08cb4d2d131575f1b01a5
liushenglin <2424044437@qq.com>
2020-11-25 15:11:51 +0800

    upload

---
+++ pages/index/index.vue
@@ -0,0 +1,42 @@
+<template>
+	<view>
+		<button type="primary" @tap="tovue">去vue页</button>
+		<button type="primary" @tap="tonvue" style="margin-top: 20rpx;">去nvue页</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+			// 这里模拟接口请求,接口请求成功后将pageLoading改为false
+			setTimeout(() => {
+				// this.pageLoading = false;
+			}, 1000);
+		},
+		methods: {
+			tovue() {
+				uni.navigateTo({
+					url: 'index3'
+				})
+			},
+			tonvue() {
+				uni.navigateTo({
+					url: 'index2'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	page {
+		height: calc(100vh - 44px);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
---
+++ pages/index/index3.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<ls-loading :nav="false" text="now loading..." v-if="pageLoading"></ls-loading>
+		<!-- 下面照常写你的页面布局 -->
+		<text>我是vue页面</text>
+	</view>
+</template>
+
+<script>
+	import lsLoading from '@/components/ls-loading/ls-loading.vue';
+	export default {
+		components: {
+			lsLoading
+		},
+		data() {
+			return {
+				pageLoading: true,
+			}
+		},
+		onLoad() {
+			// 这里模拟接口请求,接口请求成功后将pageLoading改为false
+			setTimeout(() => {
+				this.pageLoading = false;
+			}, 5000);
+		}
+	}
+</script>
+
+<style>
+	
+</style>
